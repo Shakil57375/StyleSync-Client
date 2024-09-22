f77ab1d686ff5a9c2d3a6d2366f94b5c43ac9a4a
Shakil57375 <shakil57375@gmail.com>
2024-09-22 16:06:17 +0600

    images exported

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { assets } from '../assets/assets'
 const Login = () => {
   return (
     <div>
       <p className='text-green-400'>Login</p>
+      <img src={assets.m_s_1_2} className='w-10 h-10' alt="" />
     </div>
   )
 }
---
+++ src/assets/assets.js
@@ -0,0 +1,346 @@
+import logo from "./StyleSync.png";
+import banner_1 from "./banner-1.webp";
+import banner_2 from "./banner-2.webp";
+import banner_3 from "./banner-3.webp";
+import m_s_1_1 from "./m-s-1-1.jpg";
+import m_s_1_2 from "./m-s-1-2.webp";
+import m_s_1_3 from "./m-s-1-3.webp";
+import m_s_1_4 from "./m-s-1-4.webp";
+import m_s_1_5 from "./m-s-1-5.webp";
+import m_s_2_1 from "./m-s-2-1.webp";
+import m_s_2_2 from "./m-s-2-2.webp";
+import m_s_2_3 from "./m-s-2-3.webp";
+import m_s_2_4 from "./m-s-2-4.webp";
+import m_s_2_5 from "./m-s-2-5.webp";
+import m_ts_1_1 from "./m-ts--1-1.webp";
+import m_ts_1_2 from "./m-ts-1-2.webp";
+import m_ts_1_3 from "./m-ts-1-3.webp";
+import m_ts_1_4 from "./m-ts-1-4.webp";
+import m_ts_1_5 from "./m-ts-1-5.webp";
+import k_sp_1_1 from "./k-sp-1-1.webp";
+import k_sp_1_2 from "./k-sp-1-2.webp";
+import k_sp_1_3 from "./k-sp-1-3.webp";
+import k_sp_1_4 from "./k-sp-1-4.webp";
+import w_b_1_1 from "./w-b-1-1.webp";
+import w_b_1_2 from "./w-b-1-2.webp";
+import w_b_1_3 from "./w-b-1-3.webp";
+import w_b_1_4 from "./w-b-1-4.webp";
+import w_b_1_5 from "./w-b-1-5.webp";
+import w_wc_1_1 from "./w-wc-1-1.webp";
+import w_wc_1_2 from "./w-wc-1-2.webp";
+import w_wc_1_3 from "./w-wc-1-3.webp";
+import w_wc_1_4 from "./w-wc-1-4.webp";
+import w_wc_1_5 from "./w-wc-1-5.webp";
+import k_ss_1_1 from "./k-ss-1-1.webp";
+import k_ss_1_2 from "./k-ss-1-2.webp";
+import k_ss_1_3 from "./k-ss-1-3.webp";
+import w_b_2_1 from "./w-b-2-1.webp";
+import w_b_2_2 from "./w-b-2-2.webp";
+import w_b_2_3 from "./w-b-2-3.webp";
+import w_b_2_4 from "./w-b-2-4.webp";
+import w_b_2_5 from "./w-b-2-5.webp";
+import m_wc_1_1 from "./m-wc-1-1.webp";
+import m_wc_1_2 from "./m-wc-1-2.webp";
+import m_wc_1_3 from "./m-wc-1-3.webp";
+import m_wc_1_4 from "./m-wc-1-4.webp";
+import m_cp_1_1 from "./m-cp-1-1.webp";
+import m_cp_1_2 from "./m-cp-1-2.webp";
+import m_cp_1_3 from "./m-cp-1-3.webp";
+import k_wc_1_1 from "./k-wc-1-1.webp";
+import k_wc_1_2 from "./k-wc-1-2.webp";
+import k_wc_1_3 from "./k-wc-1-3.webp";
+import k_wc_1_4 from "./k-wc-1-4.webp";
+export const assets = {
+  logo,
+  banner_1,
+  banner_2,
+  banner_3,
+  m_s_1_1,
+  m_s_1_2,
+  m_s_1_3,
+  m_s_1_4,
+  m_s_1_5,
+  m_s_2_1,
+  m_s_2_2,
+  m_s_2_3,
+  m_s_2_4,
+  m_s_2_5,
+  m_ts_1_1,
+  m_ts_1_2,
+  m_ts_1_3,
+  m_ts_1_4,
+  m_ts_1_5,
+  k_sp_1_1,
+  k_sp_1_2,
+  k_sp_1_3,
+  k_sp_1_4,
+  w_b_1_1,
+  w_b_1_2,
+  w_b_1_3,
+  w_b_1_4,
+  w_b_1_5,
+  w_wc_1_1,
+  w_wc_1_2,
+  w_wc_1_3,
+  w_wc_1_4,
+  w_wc_1_5,
+  k_ss_1_1,
+  k_ss_1_2,
+  k_ss_1_3,
+  m_cp_1_1,
+  m_cp_1_2,
+  m_cp_1_3,
+  m_wc_1_1,
+  m_wc_1_2,
+  m_wc_1_3,
+  m_wc_1_4,
+  k_wc_1_1,
+  k_wc_1_2,
+  k_wc_1_3,
+  k_wc_1_4,
+};
+
+export const products = [
+  {
+    _id: 1232148668544,
+    name: "Women's abaya with niqab",
+    description: "A very beautiful women naqab with 3 parts of protection",
+    price: 122,
+    images: [w_b_1_1, w_b_1_2, w_b_1_3, w_b_1_4],
+    category: "Women",
+    subCategory: "Fullbody",
+    sizes: ["S", "M", "L", "X", "XL"],
+    date: 1716634345448,
+    bestseller: true,
+    latest: false,
+    trending: true,
+  },
+  {
+    _id: 1232148668545,
+    name: "Men's Summer T-Shirt",
+    description:
+      "A comfortable summer t-shirt made with breathable fabric for men.",
+    price: 45,
+    images: [m_ts_1_1, m_ts_1_2, m_ts_1_3, m_ts_1_4, m_ts_1_5],
+    category: "Men",
+    subCategory: "Topware",
+    sizes: ["M", "L", "XL"],
+    date: 1716634345449,
+    bestseller: false,
+    latest: true,
+    trending: false,
+  },
+  {
+    _id: 1232148668546,
+    name: "Women's Winter Coat",
+    description: "Warm and stylish women's coat, perfect for winter wear.",
+    price: 210,
+    images: [w_wc_1_1, w_wc_1_2, w_wc_1_3, w_wc_1_4, w_wc_1_5],
+    category: "Women",
+    subCategory: "Winterware",
+    sizes: ["S", "M", "L"],
+    date: 1716634345450,
+    bestseller: true,
+    latest: false,
+    trending: true,
+  },
+  {
+    _id: 1232148668547,
+    name: "Kids' Summer Shorts",
+    description:
+      "Light and breathable shorts for kids, perfect for hot summer days.",
+    price: 30,
+    images: [k_ss_1_1, k_ss_1_2, k_ss_1_3],
+    category: "Kids",
+    subCategory: "Bottomware",
+    sizes: ["S", "M", "L"],
+    date: 1716634345451,
+    bestseller: false,
+    latest: true,
+    trending: true,
+  },
+  {
+    _id: 1232148668548,
+    name: "Women's Full-Length Abaya",
+    description: "Elegant and modest abaya for women, covering full body.",
+    price: 150,
+    images: [w_b_2_1, w_b_2_2, w_b_2_3, w_b_2_4, w_b_2_5],
+    category: "Women",
+    subCategory: "Fullbody",
+    sizes: ["S", "M", "L", "XL"],
+    date: 1716634345452,
+    bestseller: true,
+    latest: false,
+    trending: false,
+  },
+  {
+    _id: 1232148668549,
+    name: "Men's Winter Coat",
+    description: "Durable winter jacket for men with extra insulation.",
+    price: 180,
+    images: [m_wc_1_1, m_wc_1_2, m_wc_1_3, m_wc_1_4, ],
+    category: "Men",
+    subCategory: "Winterware",
+    sizes: ["M", "L", "XL"],
+    date: 1716634345453,
+    bestseller: false,
+    latest: true,
+    trending: true,
+  },
+  {
+    _id: 1232148668551,
+    name: "Men's Casual Pants",
+    description: "Comfortable casual pants for everyday wear.",
+    price: 70,
+    images: [m_cp_1_1, m_cp_1_2,m_cp_1_3],
+    category: "Men",
+    subCategory: "Bottomware",
+    sizes: ["M", "L", "XL"],
+    date: 1716634345455,
+    bestseller: false,
+    latest: true,
+    trending: false,
+  },
+  {
+    _id: 1232148668552,
+    name: "Kids' Winter Coat",
+    description: "Stylish and warm winter coat for kids.",
+    price: 95,
+    images: [k_wc_1_1, k_wc_1_2, k_wc_1_3, k_wc_1_4],
+    category: "Kids",
+    subCategory: "Winterware",
+    sizes: ["S", "M", "L"],
+    date: 1716634345456,
+    bestseller: false,
+    latest: true,
+    trending: true,
+  },
+//   {
+//     _id: 1232148668553,
+//     name: "Women's Top with Embroidery",
+//     description:
+//       "Beautifully embroidered women's top, perfect for casual outings.",
+//     price: 85,
+//     images: [w_te_1_1, w_te_1_2],
+//     category: "Women",
+//     subCategory: "Topware",
+//     sizes: ["S", "M", "L"],
+//     date: 1716634345457,
+//     bestseller: true,
+//     latest: false,
+//     trending: false,
+//   },
+//   {
+//     _id: 1232148668554,
+//     name: "Men's Formal Shirt",
+//     description: "Elegant formal shirt for men, ideal for office wear.",
+//     price: 60,
+//     images: [m_fs_1_1, m_fs_1_2],
+//     category: "Men",
+//     subCategory: "Topware",
+//     sizes: ["M", "L", "XL"],
+//     date: 1716634345458,
+//     bestseller: false,
+//     latest: true,
+//     trending: false,
+//   },
+//   {
+//     _id: 1232148668555,
+//     name: "Kids' Summer Tank Top",
+//     description: "Cool and airy tank top for kids, perfect for summer.",
+//     price: 25,
+//     images: [k_tt_1_1, k_tt_1_2],
+//     category: "Kids",
+//     subCategory: "Topware",
+//     sizes: ["S", "M", "L"],
+//     date: 1716634345459,
+//     bestseller: false,
+//     latest: true,
+//     trending: true,
+//   },
+//   {
+//     _id: 1232148668556,
+//     name: "Women's Summer Skirt",
+//     description: "Light and stylish skirt, perfect for hot summer days.",
+//     price: 55,
+//     images: [w_ss_1_1, w_ss_1_2],
+//     category: "Women",
+//     subCategory: "Bottomware",
+//     sizes: ["S", "M", "L"],
+//     date: 1716634345460,
+//     bestseller: false,
+//     latest: true,
+//     trending: true,
+//   },
+//   {
+//     _id: 1232148668557,
+//     name: "Men's Summer Shorts",
+//     description: "Breathable and comfortable men's shorts for summer.",
+//     price: 40,
+//     images: [m_ss_1_1, m_ss_1_2],
+//     category: "Men",
+//     subCategory: "Bottomware",
+//     sizes: ["M", "L", "XL"],
+//     date: 1716634345461,
+//     bestseller: false,
+//     latest: true,
+//     trending: false,
+//   },
+//   {
+//     _id: 1232148668558,
+//     name: "Women's Woolen Sweater",
+//     description: "Cozy woolen sweater for women, perfect for winter wear.",
+//     price: 120,
+//     images: [w_ws_1_1, w_ws_1_2],
+//     category: "Women",
+//     subCategory: "Winterware",
+//     sizes: ["S", "M", "L", "XL"],
+//     date: 1716634345462,
+//     bestseller: true,
+//     latest: false,
+//     trending: true,
+//   },
+//   {
+//     _id: 1232148668559,
+//     name: "Men's Full Sleeve Shirt",
+//     description:
+//       "Classic full-sleeve shirt for men, ideal for formal occasions.",
+//     price: 70,
+//     images: [m_fs_2_1, m_fs_2_2],
+//     category: "Men",
+//     subCategory: "Topware",
+//     sizes: ["M", "L", "XL"],
+//     date: 1716634345463,
+//     bestseller: false,
+//     latest: true,
+//     trending: false,
+//   },
+//   {
+//     _id: 1232148668560,
+//     name: "Kids' Winter Leggings",
+//     description: "Warm leggings for kids, great for winter comfort.",
+//     price: 35,
+//     images: [k_wl_1_1, k_wl_1_2],
+//     category: "Kids",
+//     subCategory: "Bottomware",
+//     sizes: ["S", "M", "L"],
+//     date: 1716634345464,
+//     bestseller: false,
+//     latest: true,
+//     trending: true,
+//   },
+//   {
+//     _id: 1232148668561,
+//     name: "Women's Summer Blouse",
+//     description: "Lightweight and trendy blouse for summer wear.",
+//     price: 80,
+//     images: [w_sb_1_1, w_sb_1_2],
+//     category: "Women",
+//     subCategory: "Topware",
+//     sizes: ["S", "M", "L"],
+//     date: 1716634345465,
+//     bestseller: false,
+//     latest: true,
+//     trending: true,
+//   },
+];
