81c674fce097b9a6a8bce6699606f48c3dae99f2
Shakil57375 <shakil57375@gmail.com>
2024-09-23 16:23:25 +0600

    logo added

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -43,6 +43,13 @@ const Navbar = () => {
           <CiSearch className="hover:text-gray-500 cursor-pointer text-3xl" />
           <div className="group relative">
             <FaUser className="hover:text-gray-500 cursor-pointer text-3xl"  />
+            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+                <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+                    <p className="cursor-pointer hover:text-black">My Profile</p>
+                    <p className="cursor-pointer hover:text-black">Orders</p>
+                    <p className="cursor-pointer hover:text-black">Logout</p>
+                </div>
+            </div>
           </div>
           <div className="relative">
             <IoBagOutline className="hover:text-gray-500 cursor-pointer text-3xl relative" />
