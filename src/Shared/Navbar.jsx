b36aa8ab70e0ce0b92cd01a09c212b61f36d1cdb
Shakil57375 <shakil57375@gmail.com>
2024-09-23 16:11:06 +0600

    logo added

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,17 +1,22 @@
-import { FaSearch, FaUser, FaShoppingBag } from 'react-icons/fa';
-import { assets } from '../assets/assets';
-import { Link } from 'react-router-dom';
-import ActiveLink from '../Components/ActiveLink/ActiveLink';
+import { FaUser } from "react-icons/fa";
+import { IoBagOutline } from "react-icons/io5";
+import { CiSearch } from "react-icons/ci";
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
+import ActiveLink from "../Components/ActiveLink/ActiveLink";
 const Navbar = () => {
   return (
     <nav className="w-full py-8 flex items-center justify-between ">
       {/* Logo */}
-      <Link to={"/"} className="flex items-center space-x-2 text-xl font-semibold">
-        <img src={assets.logo} alt="" className='w-16 h-16' />
+      <Link
+        to={"/"}
+        className="flex items-center space-x-2 text-xl font-semibold"
+      >
+        <img src={assets.logo} alt="" className="w-16 h-16" />
       </Link>
 
       {/* Navigation Links */}
-      <div className="flex space-x-8 text-sm font-medium">
+      <div className="flex space-x-8 text-sm font-medium items-center">
         <ActiveLink to={"/"} className="hover:text-gray-500">
           HOME
         </ActiveLink>
@@ -24,21 +29,24 @@ const Navbar = () => {
         <ActiveLink to={"/contact"} className="hover:text-gray-500">
           CONTACT
         </ActiveLink>
+        <button className="border rounded-full py-1 px-4 font-medium text-sm hover:bg-gray-100">
+          Admin Panel
+        </button>
       </div>
 
       {/* Admin Panel Button */}
       <div className="flex items-center space-x-6">
-        <button className="border rounded-full py-1 px-4 font-medium text-sm hover:bg-gray-100">
-          Admin Panel
-        </button>
+        
 
         {/* Icons */}
-        <div className="flex space-x-6 text-lg">
-          <FaSearch className="hover:text-gray-500 cursor-pointer" />
-          <FaUser className="hover:text-gray-500 cursor-pointer" />
+        <div className="flex items-center space-x-6 text-lg">
+          <CiSearch className="hover:text-gray-500 cursor-pointer text-3xl" />
+          <FaUser className="hover:text-gray-500 cursor-pointer text-3xl"  />
           <div className="relative">
-            <FaShoppingBag className="hover:text-gray-500 cursor-pointer" />
-            <span className="absolute top-0 right-0 bg-black text-white text-xs w-4 h-4 rounded-full flex items-center justify-center">0</span>
+            <IoBagOutline className="hover:text-gray-500 cursor-pointer text-3xl relative" />
+            <span className="absolute bottom-0 right-0 bg-black text-white text-xs w-4 h-4 rounded-full flex items-center justify-center">
+              0
+            </span>
           </div>
         </div>
       </div>
