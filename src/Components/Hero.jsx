936630ecb1bac8cf5a4eb6524d2a6b361dc4beb7
Shakil57375 <shakil57375@gmail.com>
2024-09-25 11:48:25 +0600

    home banner added

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,52 @@
+// src/components/BlogCarousel.jsx
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./carouselOverride.css"; // Import custom CSS to override slick styles
+import { Link } from "react-router-dom";
+import { assets } from "../assets/assets";
+const BlogCarousel = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    };
+
+    return (
+        <Slider {...settings} className="mt-8">
+                <div
+                    className="custom-slider"
+                >
+                        <img
+                            src={assets.banner_1}
+                            alt={"banner"}
+                            className="w-full h-[450px] object-cover rounded-lg"
+                        />
+                </div>
+                <div
+                    className="custom-slider"
+                >
+                        <img
+                            src={assets.banner_2}
+                            alt={"banner"}
+                            className="w-full h-[450px] object-cover rounded-lg"
+                        />
+                </div>
+                <div
+                    className="custom-slider"
+                >
+                        <img
+                            src={assets.banner_3}
+                            alt={"banner"}
+                            className="w-full h-[450px] object-cover rounded-lg"
+                        />
+                </div>
+        </Slider>
+    );
+};
+
+export default BlogCarousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Newsletter from "../Components/NewsLetter";
 import Policy from "../Components/Policy";
+import Hero from "../Components/Hero";
 
 const Home = () => {
   return (
     <div>
+      <Hero/>
       <Policy />
       <Newsletter />
     </div>
