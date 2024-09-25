28d328a6ad08f2abeac6a121d8feb0830d445fe9
Shakil57375 <shakil57375@gmail.com>
2024-09-25 21:44:21 +0600

    font added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,8 +2,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./carouselOverride.css"; // Import custom CSS to override slick styles
-import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
+
 const Hero = () => {
     const settings = {
         dots: true,
@@ -13,37 +13,59 @@ const Hero = () => {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1024, // for tablets and below
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 768, // for mobile devices
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    infinite: true,
+                    autoplay: true,
+                },
+            },
+        ],
     };
 
     return (
         <Slider {...settings} className="mt-8">
-                <div
-                    className="custom-slider"
-                >
-                        <img
-                            src={assets.banner_1}
-                            alt={"banner"}
-                            className="w-full h-[450px] object-cover rounded-lg"
-                        />
-                </div>
-                <div
-                    className="custom-slider"
-                >
-                        <img
-                            src={assets.banner_2}
-                            alt={"banner"}
-                            className="w-full h-[450px] object-cover rounded-lg"
-                        />
-                </div>
-                <div
-                    className="custom-slider"
-                >
-                        <img
-                            src={assets.banner_3}
-                            alt={"banner"}
-                            className="w-full h-[450px] object-cover rounded-lg"
-                        />
-                </div>
+            <div className="custom-slider">
+                <img
+                    src={assets.banner_1}
+                    alt={"banner"}
+                    className="w-full h-[450px] sm:h-[300px] object-cover rounded-lg"
+                />
+            </div>
+            <div className="custom-slider">
+                <img
+                    src={assets.banner_4}
+                    alt={"banner"}
+                    className="w-full h-[450px] sm:h-[300px] object-cover rounded-lg"
+                />
+            </div>
+            <div className="custom-slider">
+                <img
+                    src={assets.banner_5}
+                    alt={"banner"}
+                    className="w-full h-[450px] sm:h-[300px] object-cover rounded-lg"
+                />
+            </div>
+            <div className="custom-slider">
+                <img
+                    src={assets.banner_6}
+                    alt={"banner"}
+                    className="w-full h-[450px] sm:h-[300px] object-cover rounded-lg"
+                />
+            </div>
         </Slider>
     );
 };
