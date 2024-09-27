53b79d39f0d9b6376a0ee3d899ba94a2c5c7e0ea
Shakil57375 <shakil57375@gmail.com>
2024-09-27 17:28:06 +0600

    updated

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Collection = () => {
+  const {prodcuts} = useContext(ShopContext)
+  const [showFilter, setShowFilter] = useState(false)
   return (
-    <div>
-      <p>collection</p>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+      {/* filter Options */}
+      <div className='min-w-60'>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+        {/* Category Filter */}
+        <div className={`broder border-gray-300 pl-5 mt-6 py-3 ${showFilter ? "" : "hidden"} sm:block`}>
+          
+        </div>
+      </div>
     </div>
   )
 }
