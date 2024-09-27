ca2c1cfdb59f390025b26b24b88ed8c1e87b3cf5
Shakil57375 <shakil57375@gmail.com>
2024-09-27 17:41:31 +0600

    updated

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,21 +1,39 @@
-import React, { useContext, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import React, { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Collection = () => {
-  const {prodcuts} = useContext(ShopContext)
-  const [showFilter, setShowFilter] = useState(false)
+  const { prodcuts } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
   return (
-    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t">
       {/* filter Options */}
-      <div className='min-w-60'>
-        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS</p>
+      <div className="min-w-60">
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+          FILTERS
+        </p>
         {/* Category Filter */}
-        <div className={`broder border-gray-300 pl-5 mt-6 py-3 ${showFilter ? "" : "hidden"} sm:block`}>
-          
+        <div
+          className={`border border-gray-300 pl-5 mt-6 py-3 sm:block ${
+            showFilter ? "" : "hidden"
+          }`}
+        >
+          <p className="mb-3 text-sm font-medium">CATEGORIES</p>
+          <div className="flex flex-col gap-2 text-sm font-light text-gray-700">
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"MEN"} /> MEN
+            </p>
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"WOMEN"} /> WOMEN
+            </p>
+            <p className="flex gap-2">
+              <input type="checkbox" className="w-3" value={"KIDS"} /> KIDS
+            </p>
+          </div>
+
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Collection
+export default Collection;
