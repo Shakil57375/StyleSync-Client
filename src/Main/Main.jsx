a34656ec95fc227c863b9fca70457c457479d51d
Shakil57375 <shakil57375@gmail.com>
2024-09-28 12:34:11 +0600

    updated

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Navbar from '../Shared/Navbar'
-import { Outlet } from 'react-router-dom'
-import Footer from '../Shared/Footer'
+import React from "react";
+import Navbar from "../Shared/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer";
+import SearchBar from "../Components/SearchBar";
 
 const Main = () => {
   return (
     <div>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+      <Navbar />
+      <SearchBar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
