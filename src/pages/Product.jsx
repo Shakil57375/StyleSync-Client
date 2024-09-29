4664e6166515aa4e9eedddddbc469e2c2da5f3e4
Shakil57375 <shakil57375@gmail.com>
2024-09-29 19:48:38 +0600

    updated

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,13 +6,13 @@ const Product = () => {
   const {productId} = useParams()
   const {products} = useContext(ShopContext)
   const [productData, setProductData] = useState(false);
-
+  const [Image, setImage] = useState("");
   const fetchProductData = () =>{
     const productIdNumber = parseInt(productId, 10);
     products.map((product)=>{
       if(product._id === productIdNumber){
         setProductData(product)
-        console.log(product)
+        setImage(product.images[0])
         return null
       }
     })
@@ -22,11 +22,19 @@ const Product = () => {
     fetchProductData()
   }, [products, productId]);
 
-  return (
-    <div>
-      <p> Details</p>
+  return productData ?  (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/* Product Data */}
+      <div className='flex gap-12 flex-col sm:flex-row '>
+        {/* Product Images */}
+        <div className='flex-1 flex flex-col-reverse sm:flex-row gap-3 '>
+            <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w[18.7%] w-full'>
+              
+            </div>
+        </div>
+      </div>
     </div>
-  )
+  ) : <div className='opacity-0'></div>
 }
 
 export default Product
