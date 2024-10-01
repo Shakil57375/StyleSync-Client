c73eb6c2730c248aa75dd5dbe9d161946a36c4a5
Shakil57375 <shakil57375@gmail.com>
2024-10-01 17:34:58 +0600

    updated

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,13 +3,14 @@ import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { FaStar } from "react-icons/fa6";
 import { BsArrowRight, BsStarHalf } from "react-icons/bs";
-import '@smastrom/react-rating/style.css'
+import "@smastrom/react-rating/style.css";
 import { Rating } from "@smastrom/react-rating";
 const Product = () => {
   const { productId } = useParams();
-  const { products,currency } = useContext(ShopContext);
+  const { products, currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [Image, setImage] = useState("");
+  const [Size, setSize] = useState("");
   const fetchProductData = () => {
     const productIdNumber = parseInt(productId, 10);
     products.map((product) => {
@@ -35,12 +36,11 @@ const Product = () => {
             {productData.images.map((image) => (
               <div key={image}>
                 <img
-                onClick={() => setImage(image)}
-                className=" w-full md:w-[100px] lg:h-[120px] lg:w-[130px] md:h-[100px] h-[70px] sm:mb-3 flex-shrink-0 cursor-pointer"
-                src={image}
-                
-                alt=""
-              />
+                  onClick={() => setImage(image)}
+                  className=" w-full md:w-[100px] lg:h-[120px] lg:w-[130px] md:h-[100px] h-[70px] sm:mb-3 flex-shrink-0 cursor-pointer"
+                  src={image}
+                  alt=""
+                />
               </div>
             ))}
           </div>
@@ -52,19 +52,44 @@ const Product = () => {
         <div className="flex-1">
           <h1 className="font-medium text-2xl mt-2">{productData.name}</h1>
           <div className="flex items-center gap-1 mt-2">
-            <Rating style={{ maxWidth: 150 }} readOnly value={productData.rating}/>
+            <Rating
+              style={{ maxWidth: 150 }}
+              readOnly
+              value={productData.rating}
+            />
             <p>({productData.ratingCount})</p>
           </div>
-          <p className="mmt-5 text-3xl font-medium">{currency} {productData.price}</p>
-          <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
+          <p className="mmt-5 text-3xl font-medium">
+            {currency} {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
           <div className="flex flex-col gap-4 my-8">
-            <p className="text-green-600 font-medium">Select Size</p>
+            <p className="text-orange-600 font-medium">Select Size</p>
             <div className="flex gap-2">
-                {productData.sizes.map((productSize)=>(
-                  <button className="btn btn-outline btn-success" key={productSize}>{productSize}</button>
-                ))}
+              {productData.sizes.map((productSize) => (
+                <button
+                  onClick={() => setSize(productSize)}
+                  className={`btn ${
+                    productSize === Size ? "btn-outline btn-warning" : ""
+                  }`}
+                  key={productSize}
+                >
+                  {productSize}
+                </button>
+              ))}
             </div>
           </div>
+          <button
+            href="#_"
+            class="relative p-0.5 inline-flex items-center justify-center font-bold overflow-hidden group rounded-md"
+          >
+            <span class="w-full h-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05] absolute"></span>
+            <span class="relative px-6 py-3 transition-all ease-out bg-gray-900 rounded-md group-hover:bg-opacity-0 duration-400">
+              <span class="relative text-white">Add to cart</span>
+            </span>
+          </button>
         </div>
       </div>
     </div>
