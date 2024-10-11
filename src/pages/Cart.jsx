f801e70420006e5a774678a6211aa965fa49eace
Shakil57375 <shakil57375@gmail.com>
2024-10-11 11:52:50 +0600

    qunatity update  adeed

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,7 +4,8 @@ import Title from "../Components/Title";
 import { MdDelete } from "react-icons/md";
 
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity } =
+    useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
 
   useEffect(() => {
@@ -35,17 +36,41 @@ const Cart = () => {
             className="py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4"
           >
             <div className="flex items-start gap-6">
-             <img className="w-16 sm:w-20" src={productData.images[0]} alt="" />
-             <div>
-              <p className="text-sm sm:text-lg font-medium">{productData.name}</p>
-              <div className="flex items-center gap-5 mt-2">
-                <p>{currency}{productData.price}</p>
-                <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">{item.size}</p>
+              <img
+                className="w-16 sm:w-20"
+                src={productData.images[0]}
+                alt=""
+              />
+              <div>
+                <p className="text-sm sm:text-lg font-medium">
+                  {productData.name}
+                </p>
+                <div className="flex items-center gap-5 mt-2">
+                  <p>
+                    {currency}
+                    {productData.price}
+                  </p>
+                  <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">
+                    {item.size}
+                  </p>
+                </div>
               </div>
-             </div>
             </div>
-            <input className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1" type="text" min={1} defaultValue={item.quantity} />
-            <MdDelete onClick={() => updateQuantity(item._id, item.size, 0)} className="mr-4 cursor-pointer text-2xl text-red-600"/>
+            <input
+              onChange={(e) =>
+                e.target.value === "" || e.target.value === "0"
+                  ? null
+                  : updateQuantity(item._id, item.size, Number(e.target.value))
+              }
+              className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1"
+              type="number"
+              min={1}
+              defaultValue={item.quantity}
+            />
+            <MdDelete
+              onClick={() => updateQuantity(item._id, item.size, 0)}
+              className="mr-4 cursor-pointer text-2xl text-red-600"
+            />
           </div>
         );
       })}
